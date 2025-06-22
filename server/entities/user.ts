f992091412f78bf954dacc057e87bcbe3fd1e21f
CaoMeiYouRen <caomeiyouren@qq.com>
2025-06-22 17:08:29 +0800

    refactor(entities): 添加用户相关字段及其注释，增强可读性

--- server/entities/user.ts
+++ server/entities/user.ts
@@ -4,16 +4,46 @@ import { BaseEntity } from './base-entity'
 @Entity('user')
 export class User extends BaseEntity {
 
+    /**
+     * 用户选择的显示名称
+     */
     @Column('text', { nullable: false })
     name: string
 
+    /**
+     * 用户邮箱地址
+    */
     @Column('varchar', { length: 255, nullable: false })
     email: string
 
+    /**
+     * 用户邮箱是否已验证
+     */
     @Column('boolean', { default: false })
     emailVerified: boolean
 
+    /**
+     * 用户头像
+     */
     @Column('text', { nullable: true })
     image: string
+
+    /**
+     * 用户名
+     */
+    @Column('varchar', { length: 128, nullable: true })
+    username: string
+
+    /**
+     * 非规范化用户名
+     */
+    @Column('varchar', { length: 128, nullable: true })
+    displayUsername: string
+
+    /**
+     * 是否为匿名用户
+     */
+    @Column('boolean', { default: false })
+    isAnonymous: boolean
 
 }
